3c08abfc7cd821a1e8628e1ecdebdbdfbb797dc8
DanielDev <daniel.agudelomo@amigo.du.co>
2026-08-05 14:06:54 -0500

    first commit

---
+++ src/main/java/com/danidevbank/demo/BckdbankApplication.java
@@ -0,0 +1,13 @@
+package com.danidevbank.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BckdbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BckdbankApplication.class, args);
+    }
+
+}
